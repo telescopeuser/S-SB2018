7220b57677f29222aba9214e3559b6912e4193fd
Your Name <you@example.com>
2017-12-18 17:48:13 +0800

    update

---
+++ Day1.R
@@ -0,0 +1,34 @@
+library()
+
+print("Hello World!")
+
+## Expressions
+1+1
+"I'm learning R!"
+3^2
+
+## Logical values
+8 > 6
+1+1 == 3
+2 <= 3
+
+## source("Day1.R")
+
+## Variables
+x <- 8
+print(x)
+x / 2
+x <- "I'm learning R!"
+print(x)
+x <- 'I\'m learning R!'
+print(x)
+
+## Functions
+sum(1,2,3)
+sqrt(25)
+rep("hi!", times = 20)
+
+## Using a Package in R
+library(help='car')
+library(help="rattle")
+
