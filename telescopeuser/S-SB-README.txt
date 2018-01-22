de34e592a38951f67f7a4925297cdbf5033f8e4a
Gu Zhan <issgz@nus.edu.sg>
2018-01-22 11:41:35 +0800

    updated

---
+++ telescopeuser/S-SB-README.txt
@@ -0,0 +1,12 @@
+Data download:
+bit.ly/2hNzAoj
+
+
+WebEx:
+nus.webex.com
+
+session name:
+StatsBootCamp
+
+password:
+stats
---
+++ telescopeuser/setwd/setwd.R
@@ -0,0 +1,12 @@
+getwd()
+setwd("/media/sf_vm_shared_folder/my_github_repo/S-SB2018/telescopeuser/setwd/")
+getwd()
+
+
+
+# install.packages('ggplot2')
+library('ggplot2')
+
+example('ggplot2')
+?ggplot2
+
